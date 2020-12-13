36d27d48be7e52178db02ab32dbbff52653f6b74
Frederick Chon <frederick.chon@gmail.com>
2020-12-12 21:14:08 -0800

    added media queries at 1440px/1024px/375px for responsiveness

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+const navMobile = document.querySelector('.nav-mobile');
+const navBtn = document.querySelector('.nav-button');
+
+const hamburger = '/img/icon-hamburger.svg';
+const close = '/img/icon-close.svg';
+let toggleBtn = document.getElementById('navBtn');
+let state = false;
+
+navBtn.addEventListener('click', () => {
+    navMobile.classList.toggle('show');
+    if (state) {
+        toggleBtn.src = hamburger;
+        state = false;
+    } else {
+        toggleBtn.src = close;
+        state = true;
+    }
+});
